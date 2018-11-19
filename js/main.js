0db4311f0a726f439bc0b5632b058d9b231c86aa
LeVieuxSinge <39424362+LeVieuxSinge@users.noreply.github.com>
2018-11-19 11:20:05 -0500

    Worked on te textured and light
    Changed the fov of the camera. Added  a cursor. Added a basic wireframed sphere.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 var landscape = document.querySelector('#landscape');
 
-for (var i = 0; i < 200; i++) {
+for (var i = 0; i < 300; i++) {
     var treeNbr = Math.round(map_range(Math.random(), 0, 1, 1, 3));
     var scale = map_range(Math.random(), 0, 1, 1, 3);
-    var pX = map_range(Math.random(), 0, 1, -50, 50);
-    var pY = map_range(Math.random(), 0, 1, -50, 50);
-    var pZ = map_range(Math.random(), 0, 1, 1, 4);
+    var pX = map_range(Math.random(), 0, 1, -25, 25);
+    var pY = map_range(Math.random(), 0, 1, -25, 25);
+    var pZ = map_range(Math.random(), 0, 1, 4, 6);
     $('#landscape').append('<a-entity obj-model="obj: #tree'+ treeNbr +'-obj; mtl: #tree'+ treeNbr +'-mtl" rotation="90 0 0" scale="'+ scale +' '+ scale +' '+ scale +'" position="'+ pX +' '+ pY +' '+ pZ +'"></a-entity>');
 }
 
